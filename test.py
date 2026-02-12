13eeffa6fb9dfcfc2833bdf9464e570f4c0d29ff
Ayush212120 <ayushmishra2121@gmail.com>
2026-02-12 16:53:04 +0530

    my lab submission second modification

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from calc import  multiply, subtract
 
 assert multiply(2, 3) == 6
-assert multiply(0, 0) == 1
-assert subtract(5, 3) == 6
+assert multiply(0, 0) == 0
+assert subtract(5, 3) == 2
 assert subtract(10, 5) == 5
